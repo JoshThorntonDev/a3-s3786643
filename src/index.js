9fe194084edece1e4691c7647d9313fdd080726a
s3786643 <s3786643@student.rmit.edu.au>
2022-10-31 14:38:08 +1100

    Init local storage

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initPets } from './data/storage';
+
+initPets()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
